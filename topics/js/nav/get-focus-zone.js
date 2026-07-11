a45ca01846ac6b8a7ddd4648ca5c827bafdfde7e
tt <tt@Mac.lan>
2026-07-11 11:50:36 -0400

    main

--- topics/js/nav/get-focus-zone.js
+++ topics/js/nav/get-focus-zone.js
@@ -1,7 +1,7 @@
 // get-focus-zone.js
 export function getFocusZone({e}){
     let key = e.key
-    let zone= 'letterNavMode'
+    let zone= 'sideBar'
     if (e.target.closest('.page-header')) {zone= 'letterNavMode'}
     if (e.target.closest('.nav-lesson-title')) {zone= 'navLessonTitle'}
     if (e.target.closest('.side-bar') || e.target.id == 'sideBarBtn') {zone= 'sideBar'}
